d0065a7c774f3ce61f647e28178cd7ecb4c32b12
maniek <mpastuszka@gmail.com>
2018-07-21 12:47:42 +0200

    [add] - todo steps

---
+++ src/main/java/simpleapp/LoginDao.java
@@ -0,0 +1,12 @@
+package simpleapp;
+
+public class LoginDao {
+
+    // TODO:
+    // add maven dependency
+    // define database and fill in logins with pass
+    // implement method checkLoginAndPass
+    public boolean checkLoginAndPass(String login, String pass) {
+        return false;
+    }
+}
